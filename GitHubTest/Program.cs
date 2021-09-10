f3d4a2624cfe0b3b21d08d43e8e8ebac350b5ce2
T Wei Ming <61171208+wheyming@users.noreply.github.com>
2021-09-10 16:43:23 +0800

    New

--- GitHubTest/Program.cs
+++ GitHubTest/Program.cs
@@ -10,6 +10,8 @@ namespace GitHubTest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
